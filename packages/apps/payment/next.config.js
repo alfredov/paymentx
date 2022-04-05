14d10cb7a1b382fa4ab3e790e9e976355da5a02e
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2022-04-05 00:06:42 -0400

    try fixing http blocks entrypoint

--- packages/apps/payment/next.config.js
+++ packages/apps/payment/next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
     // outside packages/app directory. 
     externalDir: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'off'
+          }
+        ],
+      },
+    ]
+  } 
 }
 
 module.exports = nextConfig
